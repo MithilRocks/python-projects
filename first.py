f49cb676fc2b39f1fa93396c27c5d7997aee9185
Mithil Bhoras <mithilbhoras@yahoo.co.in>
2018-04-22 18:33:27 +0530

    slight changes in comments and structuring

--- first.py
+++ first.py
@@ -1,7 +1,7 @@
 # this is the main py file. import other files to use their functionalities
 from sort_list import Sort
 
+# usage of sorting 
 a = Sort([2,9,1,45,23,12,89,34,9,99,1000,978,99,67,23,45])
-
 print(a.bubble_sort())
 
--- sort_list.py
+++ sort_list.py
@@ -7,14 +7,14 @@ class Sort:
     def __init__(self, input_list):
         self.input_list = input_list
 
-    @time_taken  # calls the decorated. this will display time taken to execute the function
+    @time_taken  # calls the decorator. this will display time taken to execute the function
     # bubble sort list!
     def bubble_sort(self):
         swap = True
         while swap:
             # if no swaps occur, get out of while loop
             swap = False
-            for indx in range( 0, len(self.input_list) - 1):
+            for indx in range( 0, len(self.input_list) - 1 ):
                 if self.input_list[indx] > self.input_list[indx + 1]:
                     self.input_list[indx], self.input_list[indx + 1] = self.input_list[indx + 1], self.input_list[indx] 
                     swap = True
